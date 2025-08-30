c3a9b59ff10b9a29da111ac65d97daa91bea645c
FedericoValentino <10679810@polimi.it>
2025-08-30 12:33:04 +0200

    bash benchmark

---
+++ software_benchmark/run_benchmark.sh
@@ -0,0 +1,6 @@
+source /usr/local/share/pynq-venv/bin/activate
+source /etc/profile.d/xrt_setup.sh
+
+sudo ./c++_benchmark/re2_driver_xrt REs/c++_compare.regex INs/c++_compare.input ../cicero_compiler/re2compiler.py 1
+
+sudo -E python measure.py ../PerfCounterAxiMasterBitstreams/NEW_8x1.bit INs/c++_compare.input REs/c++_compare.regex output.txt ../cicero_compiler 10 10
